9ddbd7e9de686edc54c3a8a53ed28d50fcf9b5a2
Philip Sanders <mrcmyk12@gmail.com>
2021-09-10 09:49:51 -0700

    second fix attempt

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cookieSession = require('cookie-session');
 const passport = require('passport');
-const keys = require("./config/dev");
+const keys = require("./config/keys");
 require("./models/user");
 require("./services/passport");
 
@@ -13,7 +13,7 @@ const app = express();
 app.use(
 	cookieSession({
 		maxAge: 30 * 24 * 60 * 60 * 1000,
-		keys: [dev.cookieKey]
+		keys: [keys.cookieKey]
 	})
 );
 app.use(passport.initialize());
